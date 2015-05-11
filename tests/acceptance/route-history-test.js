e37d723a0b644571baf0ff766b37443aea589c0f
Alexander Jeurissen <alexanderjeurissen@gmail.com>
2015-05-11 14:20:22 +0200

    Add specs for route-history mixin

---
+++ tests/acceptance/route-history-test.js
@@ -0,0 +1,133 @@
+/* jshint expr:true */
+import {
+  describe,
+  it,
+  beforeEach,
+  afterEach
+} from 'mocha';
+
+import { expect } from 'chai';
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+
+var App;
+
+beforeEach(function () {
+  App = startApp();
+  visit('/');
+});
+
+afterEach(function () {
+  Ember.run(App, 'destroy');
+});
+
+describe('when directly navigating to a route, ', function () {
+  describe('it should be able to ', function () {
+    it('visit the index-route', function () {
+      visit('/');
+
+      andThen(function () {
+        expect(currentPath()).to.equal('index');
+      });
+    });
+
+    it('visit the posts-route', function () {
+      visit('/posts');
+
+      andThen(function () {
+        expect(currentPath()).to.equal('posts');
+      });
+    });
+
+    it('visit the post-route', function () {
+      visit('/post/1');
+
+      andThen(function () {
+        expect(currentPath()).to.equal('post');
+      });
+    });
+
+    it('goBack to index from posts-route', function () {
+      visit('/posts');
+
+      click('button#goBackBtn');
+
+      andThen(function () {
+        expect(currentPath()).to.equal('index');
+      });
+    });
+
+    it('goBack to index from post-route', function () {
+      visit('/post/1');
+      click('button#goBackBtn');
+      wait();
+      andThen(function () {
+        expect(currentPath()).to.equal('index');
+      });
+    });
+  });
+});
+
+describe('When transitioning between routes, ', function () {
+  describe('it should be able to transition ', function () {
+    beforeEach(function () {
+      visit('/');
+      click('a:contains("posts")');
+    });
+
+    it('index => posts', function () {
+      andThen(function () {
+        expect(currentPath()).to.equal('posts');
+      });
+    });
+
+    it('back from posts <- index', function () {
+      click('button:contains("Back")');
+      andThen(function () {
+        expect(currentPath()).to.equal('index');
+      });
+    });
+
+    it('from index -> posts', function () {
+      click('button#goBackBtn');
+      click('button#goForwardBtn');
+      andThen(function () {
+        expect(currentPath()).to.equal('posts');
+      });
+    });
+
+    it('from index <-> posts', function () {
+      click('button:contains("Back")');
+      click('button:contains("Forward")');
+      click('button:contains("Back")');
+      andThen(function () {
+        expect(currentPath()).to.equal('index');
+      });
+    });
+
+    it('from index <- posts <- post', function () {
+      click('a:contains("First")');
+      click('button#goBackBtn');
+      click('button#goBackBtn');
+      click('button#goBackBtn');
+      andThen(function () {
+        expect(currentPath()).to.equal('index');
+      });
+    });
+
+    it('from index -> posts -> post', function () {
+      click('a:contains("First")');
+      click('button#goBackBtn');
+      click('button#goBackBtn');
+      click('button#goBackBtn');
+
+      click('button#goForwardBtn');
+      click('button#goForwardBtn');
+      click('button#goForwardBtn');
+      andThen(function () {
+        expect(currentPath()).to.equal('post');
+        expect(find('#postContent').text()).to.contain('First Content');
+      });
+    });
+  });
+});
