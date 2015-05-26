8f39a2acc9c7d281085ae8634e1e168dd3276bdc
Gordon B. Isnor <gordonbisnor@gmail.com>
2015-05-26 10:34:04 -0300

    test coverage for null params issue

--- tests/acceptance/route-history-test.js
+++ tests/acceptance/route-history-test.js
@@ -131,3 +131,43 @@ describe('When transitioning between routes, ', function () {
     });
   });
 });
+
+/* transition.params null issue (More context objects were passed than there are dynamic segments for the route) */
+
+describe('it can navigate between a few routes that have null params', function () {
+  it('visits the info route ', function () {
+    visit('/');
+    click('a:contains("Info")');
+    andThen(function () {
+      expect(currentPath()).to.equal('info');
+    });
+  });
+  it('visits the info and then the about route', function () {
+    visit('/');
+    click('a:contains("Info")');
+    click('a:contains("About")');
+    andThen(function () {
+      expect(currentPath()).to.equal('about');
+    });
+  });
+  it('visits the info and about route and then goes back to the info route', function () {
+    visit('/');
+    click('a:contains("Info")');
+    click('a:contains("About")');
+    click('button#goBackBtn');
+    andThen(function () {
+      expect(currentPath()).to.equal('info');
+    });
+  });
+  it('visits the info and about route and then goes back to the info route', function () {
+    visit('/');
+    click('a:contains("Info")');
+    click('a:contains("About")');
+    click('button#goBackBtn');
+    click('button#goBackBtn');
+    andThen(function () {
+      expect(currentPath()).to.equal('index');
+    });
+  });
+});
+
