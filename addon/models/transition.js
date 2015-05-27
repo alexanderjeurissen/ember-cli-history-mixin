53a08b3b6e400b055bb17207af469f6164e57a44
Alexander Jeurissen <alexanderjeurissen@gmail.com>
2015-05-27 11:41:02 +0200

    Create new computed property to check if params are present.

--- addon/models/transition.js
+++ addon/models/transition.js
@@ -3,6 +3,10 @@ import Ember from "ember";
 export default Ember.Object.extend({
   targetRoute: null,
   params: null,
-  isSingleResource: Ember.computed.match('targetRoute', /(?:^(?!.*s\b))(?:^(?!index)).*$/)
+  isSingleResource: Ember.computed.match(
+    'targetRoute',
+    /(?:^(?!.*s\b))(?:^(?!index)).*$/
+  ),
+  hasParams: Ember.computed.bool('params')
 });
 
